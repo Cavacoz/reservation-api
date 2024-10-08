8fe2a628894d5c59519060680df6a7a4683d0cc7
Rui Cavaco <ruicavaco@hotmail.com>
2024-10-08 18:40:36 +0000

    Tests until Delete from service

--- ReservationAPITests/ReservationServiceTests.cs
+++ ReservationAPITests/ReservationServiceTests.cs
@@ -50,6 +50,64 @@ public class ReservationServiceTests
         Assert.AreEqual(2, result.Count);
     }
 
+    [TestMethod]
+    public async Task GetReservation_ReturnReservation()
+    {
+        await _dbContext.Reservations.AddAsync(new Reservation
+        {
+            ReservationName = "Rui's Reservation",
+            ReservationDay = DateOnly.FromDayNumber(0),
+            User = "Rui"
+        });
+
+        await _dbContext.SaveChangesAsync();
+
+        Reservation? reservation = await _reservationService.GetReservation(1);
+
+        Reservation expectedReservation = new()
+        {
+            ReservationName = "Rui's Reservation",
+            ReservationDay = DateOnly.FromDayNumber(0),
+            User = "Rui"
+        };
+
+        Assert.IsNotNull(reservation);
+        Assert.AreEqual(expectedReservation.ReservationName, reservation.ReservationName);
+        Assert.AreEqual(expectedReservation.ReservationDay, reservation.ReservationDay);
+        Assert.AreEqual(expectedReservation.User, reservation.User);
+    }
+
+    [TestMethod]
+    public async Task AddReservation_ReturnReservationAdded()
+    {
+        string reservationName = "Alexia's Reservation";
+        DateOnly reservationDay = DateOnly.FromDayNumber(0);
+        string user = "Alexia";
+        Reservation reservation = await _reservationService.AddReservation(reservationName, reservationDay, user);
+        Assert.IsNotNull(reservation);
+        Assert.AreEqual(reservation.ReservationID, 1);
+        Assert.AreEqual(reservation.ReservationName, reservationName);
+        Assert.AreEqual(reservation.ReservationDay, reservationDay);
+        Assert.AreEqual(reservation.User, user);
+    }
+
+    [TestMethod]
+    public async Task DeleteReservation_ReturnTrue()
+    {
+        await _dbContext.AddAsync(new Reservation
+        {
+            ReservationName = "Alexia's Reservation",
+            ReservationDay = DateOnly.FromDayNumber(0),
+            User = "Alexia"
+        });
+
+        await _dbContext.SaveChangesAsync();
+
+        bool wasDeleted = await _reservationService.DeleteReservation(1);
+
+        Assert.IsTrue(wasDeleted);
+    }
+
     [TestCleanup]
     public void Cleanup()
     {
