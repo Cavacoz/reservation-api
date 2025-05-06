c9683a5ff0e176d63568a28bbc757fa16d0cd42a
Cavaco <ruicavaco@hotmail.com>
2025-05-06 15:24:36 -0400

    RegisterDto Email Verification

--- ReservationAPI/DTO/RegisterDto.cs
+++ ReservationAPI/DTO/RegisterDto.cs
@@ -1,8 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace ReservationAPI.DTO
 {
     public class RegisterDto
     {
         public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Invalid email Address.")]
         public string Email { get; set; }
         public string Password { get; set; }
     }
