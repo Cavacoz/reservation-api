d1019a3dbb746633923a1302cca5819b6d23bf62
Cavaco <ruicavaco@hotmail.com>
2025-05-10 16:58:01 -0400

    all properties of RegisterDTO are required now

--- ReservationAPI/DTO/RegisterDto.cs
+++ ReservationAPI/DTO/RegisterDto.cs
@@ -4,11 +4,11 @@ namespace ReservationAPI.DTO
 {
     public class RegisterDto
     {
-        public string Name { get; set; }
+        public required string Name { get; set; }
 
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email must include a valid domain like example@domain.com.")]
-        public string Email { get; set; }
-        public string Password { get; set; }
+        public required string Email { get; set; }
+        public required string Password { get; set; }
     }
 }
