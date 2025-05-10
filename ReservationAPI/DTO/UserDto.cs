b30d1ebbaa152fdacd29b212703e4c4ba6293777
Cavaco <ruicavaco@hotmail.com>
2025-05-10 16:59:47 -0400

    UserDTO jwt token is required to send.

--- ReservationAPI/DTO/UserDto.cs
+++ ReservationAPI/DTO/UserDto.cs
@@ -4,6 +4,6 @@ namespace ReservationAPI.DTO
     {
         public int UserId { get; set; }
         public string Email { get; set; }
-        public string JwtToken {get;set;}
+        public required string JwtToken {get;set;}
     }
 }
