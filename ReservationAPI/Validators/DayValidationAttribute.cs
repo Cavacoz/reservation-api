17b5c2f31c3881a280afe47d5bd9de03d761abe5
Cavaco <ruicavaco@hotmail.com>
2025-05-12 14:47:26 -0400

    Day validation made use of DateTime and DateOnly Classes nad optimized class

--- ReservationAPI/Validators/DayValidationAttribute.cs
+++ ReservationAPI/Validators/DayValidationAttribute.cs
@@ -17,32 +17,29 @@ public class ValidDayAttribute : ValidationAttribute
         int year = (int)(yearProperty.GetValue(instance) ?? 0);
         int day = (int)(value ?? 0);
 
-        int maxDays = GetDaysInMonth(month, year);
-
-        if (maxDays == 0)
+        if (month > 12 || month < 1)
         {
             return new ValidationResult("Invalid month provided. Must be between 1 and 12!");
         }
 
+        int maxDays = GetDaysInMonth(year, month);
+
         if (day < 1 || day > maxDays)
         {
-            return new ValidationResult($"Day must be between 1 and {maxDays} for month {month}, year {year}.");
+            return new ValidationResult($"Day must be between 1 and {maxDays} for month {((MonthDays)month).ToString()}, year {year}.");
+        }
+
+        DateOnly reservationDay = new(year, month, day);
+        if (reservationDay.CompareTo(DateOnly.FromDateTime(DateTime.Today.AddDays(1))) < 0)
+        {
+            return new ValidationResult($"Day must be later and not earlier than the current day.");
         }
 
         return ValidationResult.Success;
     }
 
-    private int GetDaysInMonth(int month, int year)
+    private int GetDaysInMonth(int year, int month)
     {
-        return month switch
-        {
-            2 => IsLeapYear(year) ? 29 : 28,
-            4 or 6 or 9 or 11 => 30,
-            1 or 3 or 5 or 7 or 8 or 10 or 12 => 31,
-            _ => 0
-        };
+        return DateTime.DaysInMonth(year, month);
     }
-
-    private bool IsLeapYear(int year) =>
-        (year % 4 == 0) && (year % 100 != 0 || year % 400 == 0);
 }
