c35e582c1f172a441c3bfe1cc3b38245b13a1384
Cavaco <ruicavaco@hotmail.com>
2025-05-12 14:46:35 -0400

    password regular expression added

--- ReservationAPI/DTO/RegisterDto.cs
+++ ReservationAPI/DTO/RegisterDto.cs
@@ -9,6 +9,8 @@ namespace ReservationAPI.DTO
         [EmailAddress(ErrorMessage = "Invalid email address.")]
         [RegularExpression(@"^[^@\s]+@[^@\s]+\.[a-zA-Z]{2,}$", ErrorMessage = "Email must include a valid domain like example@domain.com.")]
         public required string Email { get; set; }
+        
+        [RegularExpression(@"^(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9]).{8,}$", ErrorMessage = "Password must be at least 8 characters long and include at least one uppercase letter, one number, and one special character.")]
         public required string Password { get; set; }
     }
 }
